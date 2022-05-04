22c36cc73482097e021b9665427ac01bca1ef256
Natykanaya <veronikanatykanaya@gmail.com>
2022-05-05 00:11:01 +0300

    add locators for header of home page

--- src/main/java/elements/HomePage.java
+++ src/main/java/elements/HomePage.java
@@ -1,9 +1,25 @@
 package elements;
 
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
 import elements.header.HeaderFragment;
 
-import static com.codeborne.selenide.Selenide.page;
+import static com.codeborne.selenide.Selenide.*;
 
 public class HomePage {
     protected HeaderFragment headerFragment = page(HeaderFragment.class);
+    private final SelenideElement galery = $x("//div[1]/a/img");
+    private final SelenideElement videos = $x("///div[2]/a/img");
+    private final SelenideElement infographic = $x("//div[3]/a/img");
+    private ElementsCollection topNews = $$x("//*[@id='bx-pager']/a");
+    private SelenideElement allMenu = $x("//ul[contains(@class,'b-menu-nav')]");
+    private final SelenideElement allNews = $x("//ul/li/h3/a");
+    private final SelenideElement homeMenu = $x("//i[contains(@class,'icon-home-2')]");
+    private final SelenideElement administrMenu = $x("//li/a[text()='Адміністрація']");
+    private final SelenideElement docsMenu=$x("//li/a[text()='Документи']");
+    private final SelenideElement administratorsMenu=$x("//li/a[text()='Адмінпослуги']");
+    private final SelenideElement feedbackMenu=$x("//li/a[text()='Зворотній зв’язок']");
+    private final SelenideElement accessMenu=$x("//li/a[text()='Доступ до публічної інформації']");
+    private final SelenideElement newsMenu=$x("//li/a[text()='Новини']");
+    private final SelenideElement regionMenu=$x("//li/a[text()='Про область']");
 }
--- src/main/java/elements/header/HeaderFragment.java
+++ src/main/java/elements/header/HeaderFragment.java
@@ -7,7 +7,14 @@ import static com.codeborne.selenide.Selenide.$x;
 
 @Getter
 public class HeaderFragment {
-    private SelenideElement title = $x("//p[contains(@class,'logo-text')]");
-    private SelenideElement emblem = $x("//p[contains(@class,'logo-trident')]");
-    private SelenideElement nameOfTitle = $x("//p[contains(@class,'logo') and text()='Харківська обласна державна адміністрація']");
+    private final SelenideElement title = $x("//p[contains(@class,'logo-text')]");
+    private final SelenideElement emblem = $x("//p[contains(@class,'logo-trident')]");
+    private final SelenideElement nameOfTitle = $x("//p[contains(@class,'logo') and text()='Харківська обласна державна адміністрація']");
+    private final SelenideElement iconGlasses = $x("//a[contains(@class,'sv')]");
+    private final SelenideElement ukrainianLanguage = $x("//span[text()='УКР']");
+    private final SelenideElement russianLanguage=$x("//a[text()='РУС']");
+    private final SelenideElement englishLanguage=$x("//a[text()='EN']");
+    private final SelenideElement facebookBtn=$x("//a[@title='Facebook']");
+    private final SelenideElement rssBtn=$x("//a[@title='RSS']");
+    private final SelenideElement searchBtn = $x("//input[@type='text']");
 }
